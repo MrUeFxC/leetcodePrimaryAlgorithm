9a73579fbb630c1a7c310aad0d8927b0a1758698
Yixi Liang <yixi_liang@gwu.edu>
2022-02-25 20:46:58 -0500

    Initial commit

---
+++ PrimaryAlgorithm/排列和搜索/合并两个有序数组.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        sorted = []
+        p1, p2 = 0, 0
+        while p1 < m or p2 < n:
+            if p1 == m:
+                sorted.append(nums2[p2])
+                p2 += 1
+            elif p2 == n:
+                sorted.append(nums1[p1])
+                p1 += 1
+            elif nums1[p1] < nums2[p2]:
+                sorted.append(nums1[p1])
+                p1 += 1
+            else:
+                sorted.append(nums2[p2])
+                p2 += 1
+        nums1[:] = sorted
+        return nums1
+
+
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+
+s = Solution()
+result = s.merge(nums1, m, nums2, n)
+print(result)
